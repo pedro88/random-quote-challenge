b1a7ae4fa1c1f2c32a2f18f73a0361136e33618f
pedro88 <sandrep_85@hotmail.com>
2024-10-25 20:58:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-document.querySelector('#check').addEventListener("click", () => {
-    alert("Coucou BeCode")
-    //Your code here     
-    alert("Hello!");
-  })
+window.onload = function () {
+  random();
 
-function random(){
-fetch('https://api.quotable.io/random')
-  .then(response => response.json())
-  .then(data => {
-    alert(`${data.content} —${data.author}`)
-  })
-  .catch(error => {
-    alert(error)
-  })
+  document.querySelector("#random").addEventListener("click", () => {
+    random();
+  });
+
+  document.querySelector("#copy").addEventListener("click", () => {
+    copy();
+  });
+};
+
+function random() {
+  fetch("https://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("autor").innerText = `${data.author}`;
+      document.getElementById("tag").innerText = `${data.tags}`;
+      document.getElementById("text").innerText = `"${data.content}"`;
+    })
+    .catch((error) => {
+      alert(error);
+    });
 }
 
+function copy() {
+  var copyText = document.getElementById("text");
+  copyText.select();
+  navigator.clipboard.writeText(copyText.value);
+  alert("Copied the text: " + copyText.value);
+}
